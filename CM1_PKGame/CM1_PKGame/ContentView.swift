30ef3b49522e4c1dc64529262f72ca4f3ece2f6f
Saikaku-69 <grnmnsta@gmail.com>
2024-07-31 20:40:49 +0900

    first commit

---
+++ CM1_PKGame/CM1_PKGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CM1_PKGame
+//
+//  Created by cmStudent on 2024/07/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
